1efe43cfbad79835d3a06944dceba64809a468c6
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-07-23 15:26:46 +0200

    Ajout fonction grid_creation dans la classe Grid

--- poo_bataille_navale.py
+++ poo_bataille_navale.py
@@ -3,7 +3,19 @@ class Boat:
 
 
 class Grid:
-    pass
+    def grid_creation(self, columns, lines):
+        """
+            Crée une grille 10x10 sous forme de dictionnaire.
+            Chaque case contient :
+              - boats : booléen (True si un bateau est présent)
+              - touche : booléen (True si la case a été tirée)
+              - boat_id : nom du bateau présent (ou None)
+            :return: Dictionnaire représentant la grille.
+            """
+        grid = {
+            col: {line: {"boats": False, "touche": False, "boat_id": None} for line in lines}
+            for col in columns}
+        return grid
 
 
 class Game:
