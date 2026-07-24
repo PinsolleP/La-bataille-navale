aee40020dcd677d839e24deebc5a497c90ac23f6
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-07-24 10:09:05 +0200

    Suppression ancienne fonction sunk

--- boat_class.py
+++ boat_class.py
@@ -21,22 +21,3 @@ class Boat:
 
 
 
-
-
-
-
-    #def check_sink(self, boat_name, grid):
-        """
-        Vérifie si toutes les cases d'un bateau sont touchées.
-        :param grid: Grille de jeu.
-        :param boat_name: Nom du bateau.
-        :return: True si le bateau est coulé, False sinon.
-        """
-        for col in self.coordinates:
-            for line in self.coordinates:
-                cell = self.coordinates[col][line]
-                if cell["boats"] and cell["boat_id"] == boat_name and not cell["touche"]:
-                    return False
-        return True
-
-
