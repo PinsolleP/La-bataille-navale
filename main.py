9bc35c43b0dd8c388a120ddbb99a22b64e79a4c9
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-07-24 09:45:25 +0200

    Test des relations entre Grid et Boat

--- main.py
+++ main.py
@@ -5,9 +5,19 @@
 jeux de la bataille navale
 L'utilisateur tirent sur une grille 10x10 pour tenter de couler tous les bateaux.
 """
+from grid_class import Grid
+from boat_class import Boat
 
-from bataille_navale import grid_creation, grid_display
+columns = [chr(c) for c in range(ord('A'), ord('J') + 1)]
+lines = range(1, 11)
 
 if __name__ == "__main__":
-    grid = grid_creation()
-    grid_display(grid)
+    grid = Grid(columns, lines)
+    grid.grid_display()
+
+    submarine = Boat("submarine", [("E", 9), ("F", 9)])
+
+    print(submarine.name)
+    print(submarine.get_size())
+    grid.place_boat(submarine)
+    grid.grid_display()
